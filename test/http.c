fd96ccb3a2179b395530840b81fb0c44963b4afb
Gregory Eslinger <gregjesl@gmail.com>
2021-09-06 01:22:39 -0600

    Updated windows DNS resolution

--- test/http.c
+++ test/http.c
@@ -17,16 +17,29 @@ bool condition(const char *buffer, size_t len)
 
 int main(void)
 {
-    struct hostent *server_host = NULL;
-    struct in_addr **addr_list = NULL;
     char ip[100];
     
     // Resolve the IP address
+    #ifdef WIN32
+    struct addrinfo hints;
+    struct addrinfo *result = NULL;
+    struct sockaddr_in  *sockaddr_ipv4;
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    TEST_EQUAL(getaddrinfo("example.com", "80", &hints, &result), 0);
+    sockaddr_ipv4 = (struct sockaddr_in *) result->ai_addr;
+    strcpy(ip, inet_ntoa(sockaddr_ipv4->sin_addr));
+    #else
+    struct hostent *server_host = NULL;
+    struct in_addr **addr_list = NULL;
     server_host = gethostbyname("example.com");
     TEST_NOT_NULL(server_host);
     addr_list = (struct in_addr **) server_host->h_addr_list;
     TEST_NOT_NULL(addr_list[0]);
     strcpy(ip , inet_ntoa(*addr_list[0]));
+    #endif
 
     // Create the client
     socket_session_t client = socket_session_create();
